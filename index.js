39020856d0a4b5d8f37a8c20918367bbcd942b34
Robert Gorz <rjgorz@gmail.com>
2023-01-09 09:56:39 -0600

    completed all four functions that pass test cases

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    return Math.abs(42 - value);
+}
+
+function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    const dist = distanceTravelledInFeet(start, end);
+
+    if(dist <= 400) {
+        return 0;
+    } else if(dist <= 2000) {
+        return (dist - 400) * .02;
+    } else if(dist < 2500) {
+        return 25;
+    } else if(dist >= 2500) {
+        return "cannot travel that far";
+    }
+}
